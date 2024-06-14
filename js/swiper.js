916537eff793daa96a53291b56a67651d5c0d2ea
Anna <anna.kisilevych.com>
2024-06-14 17:36:35 +0200

    Add desktop version

---
+++ js/swiper.js
@@ -0,0 +1,52 @@
+// import Swiper JS
+// import Swiper from '../swiper/swiper.min.js';
+
+
+
+
+// // core version + navigation, pagination modules:
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from '../swiper/swiper';
+// // import Swiper and modules styles
+// import 'node_modules/swiper/css';
+// import 'node_modules/swiper/css/navigation';
+// import 'node_modules/swiper/css/pagination';
+
+
+const swiper = new Swiper('.swiper-container_cards', {
+    // Стрелки
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    slideToClickedSlide: true,
+
+    // Автовысота
+    // autoHeight: true,
+
+    slidesPerView: 3,
+
+    // Отступы
+    spaceBetween: 24,
+
+    // Бесконечность
+    loop: true,
+
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnnteraction: false,  
+    // },
+    
+    // breakpoints:{
+    //     768: {
+    //         slidesPerView: 1,
+    //     },
+        // 1024: {
+        //     slidesPerView: 3,
+        // },
+        // 1440: {
+        //     slidesPerView: 3,
+        // }
+    // }
+});
