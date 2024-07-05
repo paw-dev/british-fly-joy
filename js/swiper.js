140912de58efd873e537ed7916b1fde4e24332fc
Anna <anna.kisilevych.com>
2024-07-05 17:00:42 +0200

    Added cards all versions

--- js/swiper.js
+++ js/swiper.js
@@ -12,8 +12,116 @@
 // import 'node_modules/swiper/css/navigation';
 // import 'node_modules/swiper/css/pagination';
 
+// function initializeSwiper(selector) {
+//     return new Swiper(selector, {
+//         navigation: {
+//             nextEl: '.swiper-button-next',
+//             prevEl: '.swiper-button-prev',
+//         },
+    
+//         pagination: {
+//             el: '.swiper-pagination',
+//             // Буллеты
+//             type: 'bullets',
+//             clickable: true,
+//             // hideOnClick: false,
+//             // dynamicBullets: true,
+//             // renderBullet: function (index, className) {
+//             //     return '<span class="' + className + '">' + (index + 1) + '</span>';
+//             // },
+    
+//             // Фракция
+//             // type: 'fraction',
+    
+//             // Прогрессбар
+//             // type: 'prorgessbar',
+//         },
+    
+//         // slideToClickedSlide: true,
+    
+//         // Автовысота
+//         // autoHeight: true,
+    
+//         slidesPerView: 1,
+    
+//         // Отступы
+//         spaceBetween: 10,
+    
+//         // Бесконечность
+//         loop: true,
+    
+//         // autoplay: {
+//         //     delay: 3000,
+//         //     disableOnnteraction: false,  
+//         // },
+        
+//         breakpoints:{
+//             1440: {
+//                 slidesPerView: 3,
+//             },
+//             768: {
+//                 slidesPerView: 2,
+//             },
+//             480: {
+//                 slidesPerView: 1,
+//             },
+            
+            
+//         }
+//     });
+// }
 
-const swiper = new Swiper('.swiper-container_cards_lm', {
+// const swiper1 = initializeSwiper('.swiper-container_cards_lm');
+// const swiper2 = initializeSwiper('.swiper-container_cards_cruises');
+
+
+const swiper_lm = new Swiper('.swiper-container_cards_lm',  {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+
+    // Автовысота
+    // autoHeight: true,
+
+    slidesPerView: 1,
+
+    // Отступы
+    spaceBetween: 10,
+
+    // Бесконечность
+    loop: true,
+
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnnteraction: false,  
+    // },
+    
+    breakpoints:{
+         320: {
+            slidesPerView: 1,
+        },
+             700: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+   
+       
+        
+        
+    }
+});
+
+
+const swiper_cruises = new Swiper('.swiper-container_cards_cruises',  {
     // Стрелки
     navigation: {
         nextEl: '.swiper-button-next',
@@ -57,16 +165,78 @@ const swiper = new Swiper('.swiper-container_cards_lm', {
     // },
     
     breakpoints:{
-        1440: {
-            slidesPerView: 3,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        480: {
-            slidesPerView: 1,
-        },
-        
-        
+        320: {
+           slidesPerView: 1,
+       },
+            700: {
+           slidesPerView: 2,
+       },
+       1215: {
+           slidesPerView: 3,
+       },
     }
+});
+const swiper_villas = new Swiper('.swiper-container_cards_villas',  {
+    // Стрелки
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+
+    pagination: {
+        el: '.swiper-pagination',
+        // Буллеты
+        type: 'bullets',
+        clickable: true,
+        // hideOnClick: false,
+        // dynamicBullets: true,
+        // renderBullet: function (index, className) {
+        //     return '<span class="' + className + '">' + (index + 1) + '</span>';
+        // },
+
+        // Фракция
+        // type: 'fraction',
+
+        // Прогрессбар
+        // type: 'prorgessbar',
+    },
+
+    // slideToClickedSlide: true,
+
+    // Автовысота
+    // autoHeight: true,
+
+    // slidesPerView: 1,
+    centeredSlides: true,
+
+    slidesPerView: "auto", 
+
+    // Отступы
+    spaceBetween: 30,
+
+    // Бесконечность
+    // loop: true,
+
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnnteraction: false,  
+    // },
+    
+    // breakpoints:{
+    //     1400: {
+    //         slidesPerView: 2,
+    //     },
+    //     1440: {
+    //         slidesPerView: 1.25,
+    //     },
+        
+        // 768: {
+        //     slidesPerView: 2,
+        // },
+        // 480: {
+        //     slidesPerView: 1,
+        // },
+        
+        
+    // }
 });
